2db40d59c2d97487cd4934027d02f9f0e4183afd
Bibekdeep Singh <bibekdeepsingh@rrc.ca>
2024-09-25 14:20:43 -0500

    Created shape class and completed the shape.py file

--- shape/shape.py
+++ shape/shape.py
@@ -1,5 +1,36 @@
 """"
 Description: A class to represent generic Shape objects.
-Author: {Student Name}
-Date: {Date}
-"""
+Author: Bibekdeep Singh
+Date: 25-09-24
+""" 
+
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    """
+    A class which represents shape objects.
+
+    Attributes:
+        color(str): Represents the color of the shape.
+
+    Methods:
+        calculate_area(): (Abstract) Calculates the area of the shape.
+        calculate_perimeter(): (Abstract) Calculates the perimeter of the shape.
+    """
+
+    def __init__(self, color: str):
+        if len(color.strip()) == 0:
+            raise ValueError("Color cannot be blank.")
+        else:
+            self._color = color
+
+    def __str__(self) -> str:
+        return f"The shape color is {self._color}."
+    
+    @abstractmethod
+    def calculate_area(self) -> float:
+        pass
+
+    @abstractmethod
+    def calculate_perimeter(self) -> float:
+        pass
